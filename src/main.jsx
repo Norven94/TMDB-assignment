8feb6d1823d6648cd1ec22a201a62b8a13f26626
Oskar Anderberg <otto.anderberg@gmail.com>
2021-09-07 23:22:19 +0200

    install react-query and add it to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import './App.scss'
 import App from './App'
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 1000 * 60 * 60, // Fetch new data after 1h 
+		},
+	},
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )

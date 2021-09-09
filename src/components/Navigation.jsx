becab53a01408cf8c28e6d8380a9b2cb00759155
Oskar Anderberg <otto.anderberg@gmail.com>
2021-09-09 11:28:23 +0200

    Fix navigation layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,24 +3,23 @@ import { Link, NavLink } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import "../scss/navigation.scss";
 
 const Navigation = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="md">
+        <Navbar expand="lg" className="navbar">
             <Container>
-                <Link to="/" className="navbar-brand">Movie Library</Link>
+                <Navbar.Brand>
+                    <Link to="/" className="navbar-brand">Movie Library</Link>
+                </Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="ms-auto">
-                            <NavDropdown titel="Movies" id="basic-nav-dropdown">
-                                <NavLink to="/movies" className="dropdown-item">Filter Movies</NavLink>
-                                <NavDropdown.Divider />
-                                <NavLink to="/top-movies" className="dropdown-item">Top Movies</NavLink>
-                                <NavLink to="/popular-movies" className="dropdown-item">Popular Movies</NavLink>
-                                <NavLink to="/cinema-movies" className="dropdown-item">Cinema Movies</NavLink>
-                            </NavDropdown>
+						<Nav className="me-auto">
+                            <NavLink to="/movies" className="dropdown-item">Filter Movies</NavLink>
+                            <NavLink to="/top-movies" className="dropdown-item">Top Movies</NavLink>
+                            <NavLink to="/popular-movies" className="dropdown-item">Popular Movies</NavLink>
+                            <NavLink to="/cinema-movies" className="dropdown-item">Cinema Movies</NavLink>
                         </Nav>
                     </Navbar.Collapse>
             </Container>

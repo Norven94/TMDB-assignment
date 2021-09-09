79ec55d707e7837f0c5a78f1aabc446cf1447ce3
Oskar Anderberg <otto.anderberg@gmail.com>
2021-09-09 11:28:01 +0200

    Add multiple pages to pagination
    Add multiple pages to pagination so that you can instantly jump from for example page 1 to page 5.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
+import React, { useState, useEffect } from 'react'
 
 const Pagination = ({isPreviousData, totalPages, page, setPage}) => {
+    const [ pages, setPages ] = useState([page - 1, page, page + 1, page + 2 ,page + 3])
+
+    //useEffect to avoid render to many pages and also move current page indicator
+    useEffect(() => {
+        if (page === 1) {
+            setPages([page, page + 1, page + 2, page + 3, page + 4 ])
+        } else if (page + 2 === totalPages) {
+            setPages([page - 2, page - 1, page , page + 1 ,page + 2])
+        } else if (page + 1 === totalPages) {
+            setPages([page - 3, page - 2, page - 1 ,page ,page + 1])
+        } else if (page === totalPages) {
+            setPages([page - 4, page - 3, page - 2 ,page - 1 ,page])
+        } else {
+            setPages([page - 1, page, page + 1, page + 2 ,page + 3])
+        }
+    }, [page])
+
     return (
-        <div>
-            <Button
+        <div className="pagination-container">
+            <button
+                className="primary-btn"
                 onClick={() => setPage(currentPage => Math.max(currentPage - 1, 1))}
                 disabled={page === 1}
             >
                 Previous Page
-            </Button>
+            </button>
 
-            <span>Current Page: {page}</span>
+            {pages.map((pageNum, i) => {
+                if (pageNum === page) {
+                    return ( 
+                        <span 
+                            className="page current" 
+                            key={i} 
+                            onClick={() => {
+                                setPage(pageNum)
+                            }} 
+                        >{pageNum}</span> 
+                    )
+                } else {
+                    return ( 
+                        <span 
+                            className="page" 
+                            key={i} 
+                            onClick={() => {
+                                setPage(pageNum)
+                            }} 
+                        >{pageNum}</span> 
+                    )
+                }
+            })}
 
-            <Button
+            <button
+                className="primary-btn"
                 onClick={() => {
                     if (!isPreviousData && page < totalPages) {
                         setPage(currentPage => Math.max(currentPage + 1))
@@ -22,7 +62,7 @@ const Pagination = ({isPreviousData, totalPages, page, setPage}) => {
                 disabled={isPreviousData || totalPages === page}
             >
                 Next Page
-            </Button>
+            </button>
         </div>
     )
 }
